c72aac7dec55ab20b8ddb9a442c050e5ebe0879e
Makoto Ishibashi <arsoncase@gmail.com>
2023-04-29 17:07:35 +0900

    UserEntityTest作成

---
+++ TDD_CleanArchitecture/test/com/mishibashi/tdd/domain/model/UserEntityTest.java
@@ -0,0 +1,19 @@
+package com.mishibashi.tdd.domain.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserEntityTest {
+
+	@Test
+	void 大文字に変換した名前を取得する() {
+		User user = new User("0001", "Makoto Ishibashi");
+		String expectedString = "MAKOTO ISHIBASHI";
+		
+		UserEntity userEntity = new UserEntity(user);
+		
+		assertEquals(expectedString, userEntity.getUpperName());
+	}
+
+}
