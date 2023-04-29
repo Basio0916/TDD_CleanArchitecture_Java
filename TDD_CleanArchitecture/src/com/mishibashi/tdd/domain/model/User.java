009b4e5712aab9553e5e702edc002a99684cdc4f
Makoto Ishibashi <arsoncase@gmail.com>
2023-04-29 16:28:31 +0900

    Userクラス追加

---
+++ TDD_CleanArchitecture/src/com/mishibashi/tdd/domain/model/User.java
@@ -0,0 +1,20 @@
+package com.mishibashi.tdd.domain.model;
+
+public class User {
+	private final String _id;
+	
+	private final String _name;
+	
+	public User(String id, String name) {
+		_id = id;
+		_name = name;
+	}
+	
+	public String getId() {
+		return _id;
+	}
+	
+	public String getName() {
+		return _name;
+	}
+}
